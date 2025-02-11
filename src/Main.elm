9956c27b32901d076557de9407e2ea0f548e6ad7
James Curtis <mybastion@gmail.com>
2025-02-11 19:52:35 +0000

    feat(project): Add faction to model

--- src/Main.elm
+++ src/Main.elm
@@ -4,32 +4,47 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
+
 main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model = Int
+
+type Faction
+    = IronSamurai
+    | TheSyndicate
+
+
+type alias Model =
+    { faction : Maybe Faction
+    , count : Int
+    }
+
 
 init : Model
 init =
-  0
+    { faction = Nothing, count = 0 }
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            { model | count = model.count - 1 }
 
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
